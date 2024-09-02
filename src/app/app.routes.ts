326aff89de21605b01de620f80b367e2d325da56
ramon senger <51542841+rcsCrew@users.noreply.github.com>
2024-09-02 13:43:51 -0300

    add

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { HubComponent } from './pages/hub/hub.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: "", redirectTo: "hub", pathMatch: "full"},
+  {path: "hub", component: HubComponent}
+];
